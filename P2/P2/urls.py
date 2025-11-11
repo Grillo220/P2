8fb106710519263c42a8e81ae45ae82ad222f5df
Grillo220 <carlosrodrigovergara1@gmail.com>
2025-11-11 13:40:25 +0000

    f

--- P2/P2/urls.py
+++ P2/P2/urls.py
@@ -21,3 +21,12 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('addon1/', include('addon1.urls')),
+    path('addon2/', include('addon2.urls')),
+]
